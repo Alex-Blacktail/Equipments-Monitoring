cc6119189c26e4841972a046f7b0b51036d43f50
Алексей Попов <a.popov@w1.money>
2022-07-21 23:42:07 +0500

    Добавлен интерфейс взаимодействия с бд

---
+++ src/Equipments.Application/Interfaces/IEquipmentsDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Equipments.Domain;
+
+namespace Equipments.Application.Interfaces
+{
+    public interface IEquipmentsDbContext
+    {
+        DbSet<Account> Accounts { get; set; }
+        DbSet<Equipment> Equipments { get; set; }
+        DbSet<EquipmentComponent> EquipmentComponents { get; set; }
+        DbSet<EquipmentComponentType> EquipmentComponentTypes { get; set; }
+        DbSet<EquipmentType> EquipmentTypes { get; set; }
+        DbSet<PersonInfo> PersonInfos { get; set; }
+        DbSet<PropertyOfEquipmentComponent> PropertyOfEquipmentComponents { get; set; }
+        DbSet<PropertyType> PropertyTypes { get; set; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
