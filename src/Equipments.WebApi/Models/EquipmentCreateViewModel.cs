fd455b83576796c2ab039844c4147b499ffe573a
Алексей Попов <a.popov@w1.money>
2022-07-21 23:50:47 +0500

    Добавлены модели для определения получаемых с веб морды данных

---
+++ src/Equipments.WebApi/Models/EquipmentCreateViewModel.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using Equipments.Application.Commands;
+using Equipments.Application.Common.Mappings;
+
+namespace Equipments.WebApi.Models
+{
+    public class EquipmentCreateViewModel : IMapWith<EquipmentCreateCommand>
+    {
+        public string Name { get; set; }
+        public string ModelNumber { get; set; }
+        public string InventoryNumber { get; set; }
+        public DateTime ProductionDate { get; set; }
+        public DateTime ReceiptDate { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<EquipmentCreateViewModel, EquipmentCreateCommand>()
+               .ForMember(vm => vm.Name, e => e.MapFrom(e => e.Name))
+               .ForMember(vm => vm.ModelNumber, e => e.MapFrom(e => e.ModelNumber))
+               .ForMember(vm => vm.InventoryNumber, e => e.MapFrom(e => e.InventoryNumber))
+               .ForMember(vm => vm.ProductionDate, e => e.MapFrom(e => e.ProductionDate))
+               .ForMember(vm => vm.ReceiptDate, e => e.MapFrom(e => e.ReceiptDate));
+        }
+    }
+}
