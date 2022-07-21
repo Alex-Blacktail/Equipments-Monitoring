d20098adfc50dd73fba7b633a05bbd4068562060
Алексей Попов <a.popov@w1.money>
2022-07-21 23:44:59 +0500

    Добавлены комманды для работы с типами оргтехники

---
+++ src/Equipments.Application/Contracts/Commands/EquipmentTypeCommands.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+
+namespace Equipments.Application.Contracts.Commands
+{
+    public class EquipmentTypeCreateCommand : IRequest<int>
+    {
+        public string Name { get; set; }
+    }
+
+    public class EquipmentTypeUpdateCommand : IRequest
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class EquipmentTypeDeleteCommand : IRequest
+    {
+        public int Id { get; set; }
+    }
+}
