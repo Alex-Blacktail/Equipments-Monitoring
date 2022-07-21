1bd89588ba99f4e38af9b1253477db0ce73475a8
Алексей Попов <a.popov@w1.money>
2022-07-21 23:43:01 +0500

    Добавлены обработчики комманд и запросов для работы с оргтехникой

---
+++ src/Equipments.Application/Handlers/EquipmentCommandHandler.cs
@@ -0,0 +1,79 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Equipments.Domain;
+using Equipments.Application.Interfaces;
+using Equipments.Application.Common.Exeptions;
+using Microsoft.EntityFrameworkCore;
+using MediatR;
+using Equipments.Application.Commands;
+
+namespace Equipments.Application.Handlers
+{
+    public class EquipmentCommandHandler :
+        IRequestHandler<EquipmentCreateCommand, int>,
+        IRequestHandler<EquipmentUpdateCommand>,
+        IRequestHandler<EquipmentDeleteCommand>
+    {
+        private readonly IEquipmentsDbContext _context;
+
+        public EquipmentCommandHandler(IEquipmentsDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> Handle(EquipmentCreateCommand request, CancellationToken cancellationToken)
+        {
+            var equipment = new Equipment
+            {
+                InventoryNumber = request.InventoryNumber,
+                ProductionDate = request.ProductionDate,
+                ReceiptDate = request.ReceiptDate,
+                ModelNumber = request.ModelNumber,
+                Name = request.Name
+            };
+
+            await _context.Equipments.AddAsync(equipment, cancellationToken);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return equipment.Id;
+        }
+
+        public async Task<Unit> Handle(EquipmentUpdateCommand request, CancellationToken cancellationToken)
+        {
+            var entity = await _context.Equipments
+                .FirstOrDefaultAsync(e => e.Id == request.Id);
+
+            if (entity == null)
+            {
+                throw new NotFoundExeption(nameof(Equipment), request.Id);
+            }
+
+            entity.Name = request.Name;
+            entity.ProductionDate = request.ProductionDate;
+            entity.ReceiptDate = request.ReceiptDate;
+            entity.InventoryNumber = request.InventoryNumber;
+            entity.ModelNumber = request.ModelNumber;
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+
+        public async Task<Unit> Handle(EquipmentDeleteCommand request, CancellationToken cancellationToken)
+        {
+            var entity = await _context.Equipments
+                .FirstOrDefaultAsync(e => e.Id == request.Id);
+
+            if (entity == null)
+            {
+                throw new NotFoundExeption(nameof(Equipment), request.Id);
+            }
+
+            _context.Equipments.Remove(entity);
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
---
+++ src/Equipments.Application/Handlers/EquipmentQueryHandler.cs
@@ -0,0 +1,49 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Equipments.Application.Interfaces;
+using Equipments.Application.ViewModels;
+using Equipments.Application.Common.Exeptions;
+using MediatR;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Equipments.Application.Queries;
+
+namespace Equipments.Application.Handlers
+{
+    public class EquipmentQueryHandler :
+        IRequestHandler<EquipmentGetDetailsQuery, EquipmentDetailsViewModel>,
+        IRequestHandler<EquipmentGetListQuery, EquipmentsListViewModel>
+    {
+        private readonly IEquipmentsDbContext _context;
+        private readonly IMapper _mapper;
+
+        public EquipmentQueryHandler(IEquipmentsDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<EquipmentDetailsViewModel> Handle(EquipmentGetDetailsQuery request, CancellationToken cancellationToken)
+        {
+            var equipment = await _context.Equipments
+                .FirstOrDefaultAsync(e => e.Id == request.Id, cancellationToken);
+
+            if (equipment == null)
+            {
+                throw new NotFoundExeption(nameof(equipment), request.Id);
+            }
+
+            return _mapper.Map<EquipmentDetailsViewModel>(equipment);
+        }
+
+        public async Task<EquipmentsListViewModel> Handle(EquipmentGetListQuery request, CancellationToken cancellationToken)
+        {
+            var equipments = await _context.Equipments
+                .ProjectTo<EquipmentLookupViewModel>(_mapper.ConfigurationProvider)
+                .ToListAsync();
+
+            return new EquipmentsListViewModel { Equipments = equipments };
+        }
+    }
+}
