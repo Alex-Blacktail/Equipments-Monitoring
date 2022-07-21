7bd640d077d0282b12b106b556143339d2060328
Алексей Попов <a.popov@w1.money>
2022-07-21 23:49:51 +0500

    Внедрены зависимости в WebApi для маппера, медиатора и бд, подключен сваггер

--- src/Equipments.WebApi/Program.cs
+++ src/Equipments.WebApi/Program.cs
@@ -1,6 +1,31 @@
+using System.Reflection;
+using Equipments.Application;
+using Equipments.Application.Common.Mappings;
+using Equipments.Application.Interfaces;
+using Equipments.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAutoMapper(c =>
+{
+    c.AddProfile(new AssemblyMappingProfile(Assembly.GetExecutingAssembly()));
+    c.AddProfile(new AssemblyMappingProfile(typeof(IEquipmentsDbContext).Assembly));
+});
+
+builder.Services.AddMediatr();
+builder.Services.AddEquipmentsDbContext(builder.Configuration);
+
+builder.Services.AddControllers();
+
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseRouting();
+app.UseHttpsRedirection();
+app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
 
 app.Run();
