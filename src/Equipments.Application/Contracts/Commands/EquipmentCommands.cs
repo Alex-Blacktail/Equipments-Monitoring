caf111b1844d6c0b85fb58b53c460876f053d6a0
Алексей Попов <a.popov@w1.money>
2022-07-21 23:42:32 +0500

    Добавлены комманды и запросы для работы с оргтехникой

---
+++ src/Equipments.Application/Contracts/Commands/EquipmentCommands.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using MediatR;
+
+namespace Equipments.Application.Commands
+{
+    public class EquipmentCreateCommand : IRequest<int>
+    {
+        public string Name { get; set; }
+        public string ModelNumber { get; set; }
+        public string InventoryNumber { get; set; }
+        public DateTime ProductionDate { get; set; }
+        public DateTime ReceiptDate { get; set; }
+    }
+
+    public class EquipmentUpdateCommand : IRequest
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ModelNumber { get; set; }
+        public string InventoryNumber { get; set; }
+        public DateTime ProductionDate { get; set; }
+        public DateTime ReceiptDate { get; set; }
+    }
+
+    public class EquipmentDeleteCommand : IRequest
+    {
+        public int Id { get; set; }
+    }
+}
+    
---
+++ src/Equipments.Application/Contracts/Queries/EquipmentQueries.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using MediatR;
+using Equipments.Application.ViewModels;
+
+namespace Equipments.Application.Queries
+{
+    public class EquipmentGetDetailsQuery : IRequest<EquipmentDetailsViewModel>
+    {
+        public int Id { get; set; }
+    }
+
+    public class EquipmentGetListQuery : IRequest<EquipmentsListViewModel>
+    {
+    }
+}
