2bd7e168181afb2858459243041818683d149b41
Алексей Попов <a.popov@w1.money>
2022-07-21 23:44:17 +0500

    Добавлен метод расширения для внедрения зависимостей, который подрубает медиатор

---
+++ src/Equipments.Application/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MediatR;
+using System.Reflection;
+
+namespace Equipments.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddMediatr(this IServiceCollection services)
+        {
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            return services;
+        }
+    }
+}
